78554b0e618ce1af86880f5048ee23fa1907cf4a
ogbk <ogbu.k.olu@gmail.com>
2022-09-07 22:49:15 +0100

    add second plusOne function

--- recursion/plusOne.js
+++ recursion/plusOne.js
@@ -1,8 +1,8 @@
-const plusOne = (array) => {
+// Simple function
+const plusOne1 = (array) => {
   const rec = (arr, savedArrayLength) => {
 
-    // CASE 1
-    // arr = []
+    // CASE 1 -> arr = []
     if(arr.length === 0) {
       return {
         shift: true,
@@ -18,8 +18,7 @@ const plusOne = (array) => {
 
     const {shift, value} = rec(arr);
 
-    // CASE 2
-    // item is first array element
+    // CASE 2 -> item is first array element
     if(fullArrayLength === savedArrayLength) {
       if (!shift) {
         return ([item].concat(value))
@@ -30,8 +29,7 @@ const plusOne = (array) => {
       return ([1,0].concat(value))
     }
 
-    // CASE 3
-    // array is [0,1,2,...,n] -> item in [1,...,n]
+    // CASE 3 -> array is [0,1,2,...,n] -> item in [1,...,n]
     if (!shift) {
       return {
         value: [item].concat(value),
@@ -85,4 +83,21 @@ const plusOne = (array) => {
   }
 }
 
-module.exports = plusOne
+// Longer function
+const plusOne2 = (array) => {
+  const rec = (remainder, result) => {
+    if (remainder.length === 0) {
+      return ([1].concat(result));
+    }
+
+    const lastElem = remainder.pop();
+    if (lastElem < 9) {
+      return (remainder.concat(lastElem+1, result));
+    }
+    return rec(remainder, [0].concat(result))
+  }
+
+  return rec(array, [])
+}
+
+module.exports = { plusOne1, plusOne2 }
